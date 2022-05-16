e7719aa85c5107b4f86721ccaf36df74b2ef1d9e
aresous <aresous@gmail.com>
2022-05-16 19:51:33 -0400

    initialized

---
+++ pages/signIn.js
@@ -0,0 +1,109 @@
+import {
+  getCsrfToken,
+  getProviders,
+  signIn,
+  getSession,
+} from "next-auth/react";
+import Router from "next/router";
+import React from "react";
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+export default function signInPage({ csrfToken, providers }) {
+  const [email, setEmail] = useState(""); // might be an error cause i have 2 email forms / ids / names below. watch the vid maybe he fixes it idk.
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState(null);
+  const { data: session } = useSession();
+  const signInUser = async (e) => {
+    e.preventDefault();
+    let options = { redirect: false, email, password };
+    console.log(options);
+    const res = await signIn("credentials", options);
+    setMessage(null);
+    if (res?.error) {
+      setMessage(res.error);
+    } else {
+      return Router.push("/");
+    }
+    console.log(email, password);
+  };
+
+  const signupUser = async (e) => {
+    e.preventDefault();
+    setMessage(null);
+    const res = await fetch("/api/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+    let data = await res.json();
+    if (data.message) {
+      setMessage(data.message);
+    }
+
+    if (data.message === "Registered successfully") {
+      let options = { redirect: false, email, password };
+      const res = await signIn("credentials", options);
+      return Router.push("/");
+    }
+  };
+  return (
+    <>
+      {session ? <p>session</p> : "no session"}
+      {/* <form method="post" action="api/auth/signin/email">
+        <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
+        <label>
+          Email address
+          <input type="email" id="email" name="email" />
+        </label>
+        <button type="submit">Sign in with email</button>
+      </form> */}
+      <form action="">
+        <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
+        <label>
+          Email address
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+        </label>
+        <label>
+          Password
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+        </label>
+        <p style={{ color: "red" }}>{message}</p>
+        <button onClick={(e) => signInUser(e)}>Sign in with credentials</button>
+        <button onClick={(e) => signupUser(e)}>Sign up</button>
+      </form>
+    </>
+  );
+}
+export async function getServerSideProps(context) {
+  console.log(context);
+  const { req } = context;
+  const session = await getSession({ req });
+  if (session) {
+    return {
+      redirect: { destination: "/" },
+    };
+  }
+  const csrfToken = await getCsrfToken(context);
+  const providers = await getProviders();
+  return {
+    props: { csrfToken, providers },
+  };
+}
